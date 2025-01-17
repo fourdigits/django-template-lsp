d4e7ff4108c57f147f74f06a5191da78d8e062cc
Maikel Martens <maikel@fourdigits.nl>
2025-01-17 10:04:27 +0100

    Add cli arguments to set initialization options

--- djlsp/cli.py
+++ djlsp/cli.py
@@ -13,6 +13,24 @@ def main():
         version=f"%(prog)s {__version__}",
     )
     parser.add_argument("--enable-log", action="store_true")
+    parser.add_argument(
+        "--docker-compose-file",
+        type=str,
+        default="docker-compose.yml",
+        help="Path to the docker-compose.yml file",
+    )
+    parser.add_argument(
+        "--docker-compose-service",
+        type=str,
+        default="django",
+        help="Docker Compose service name for Django",
+    )
+    parser.add_argument(
+        "--django-settings-module",
+        type=str,
+        default="",
+        help="Django settings module. If left empty, autodetection will be attempted.",
+    )
 
     args = parser.parse_args()
 
@@ -22,5 +40,13 @@ def main():
         file_handler = logging.FileHandler("djlsp.log")
         file_handler.setLevel(logging.DEBUG)
         logger.addHandler(file_handler)
+
+    server.set_initialization_options(
+        {
+            "docker_compose_file": args.docker_compose_file,
+            "docker_compose_service": args.docker_compose_service,
+            "django_settings_module": args.django_settings_module,
+        }
+    )
 
     server.start_io()
