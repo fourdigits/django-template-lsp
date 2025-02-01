7cbf163bd1dea6cdfd3714b9f063f4c63d8c1ef2
Maikel Martens <maikel@martens.me>
2025-02-01 15:42:30 +0100

    Add hover and goto test + fix goto template not working on single quote

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,5 @@
 import jedi
+import pytest
 from pygls.workspace import TextDocument
 
 from djlsp.constants import FALLBACK_DJANGO_DATA
@@ -7,7 +8,7 @@ from djlsp.parser import TemplateParser
 
 
 def create_parser(source) -> TemplateParser:
-    workspace_index = WorkspaceIndex()
+    workspace_index = WorkspaceIndex(src_path="/project/src", env_path="/project/env")
     workspace_index.update(
         {
             "static_files": ["js/main.js", "css/main.css"],
@@ -29,10 +30,18 @@ def create_parser(source) -> TemplateParser:
                 "__builtins__": FALLBACK_DJANGO_DATA["libraries"]["__builtins__"],
                 "website": {
                     "tags": {
-                        "get_homepage": {},
+                        "get_homepage": {
+                            "name": "get_homepage",
+                            "docs": "Retrieve the homepage.",
+                            "source": "src:templatetags/website.py:8",
+                        },
                     },
                     "filters": {
-                        "currency": {},
+                        "currency": {
+                            "name": "currency",
+                            "docs": "Formats a number as currency.",
+                            "source": "src:filters.py:5",
+                        },
                     },
                 },
             },
@@ -159,3 +168,75 @@ def test_completion_context_based_type_hint_comment():
     parser = create_parser("{# type news: str #}\n{{ news.cap")
     assert any(item.label == "news" for item in parser.completions(1, 5))
     assert any(item.label == "capitalize" for item in parser.completions(1, 10))
+
+
+###################################################################################
+# Hovers
+###################################################################################
+def test_hover_url():
+    parser = create_parser("{% url 'website:home' %}")
+    hover = parser.hover(0, 12)
+    assert hover is not None
+    assert hover.contents == "Homepage"
+
+
+def test_hover_filter():
+    parser = create_parser("{% load website %}\n{{ some_variable|currency }}")
+    hover = parser.hover(1, 23)
+    assert hover is not None
+    assert hover.contents == "Formats a number as currency."
+
+
+def test_hover_tag():
+    parser = create_parser("{% load website %}\n{% get_homepage %}")
+    hover = parser.hover(1, 12)
+    assert hover is not None
+    assert hover.contents == "Retrieve the homepage."
+
+
+###################################################################################
+# Goto Definitions
+###################################################################################
+def test_goto_definition_url():
+    parser = create_parser("{% url 'website:home' %}")
+    definition = parser.goto_definition(0, 12)
+    assert definition is not None
+    assert definition.uri == "file:///project/src/views.py"
+    assert definition.range.start.line == 12
+    assert definition.range.start.character == 0
+
+
+def test_goto_definition_filter():
+    parser = create_parser("{% load website %}\n{{ some_variable|currency }}")
+    definition = parser.goto_definition(1, 25)
+    assert definition is not None
+    assert definition.uri == "file:///project/src/filters.py"
+    assert definition.range.start.line == 5
+    assert definition.range.start.character == 0
+
+
+def test_goto_definition_tag():
+    parser = create_parser("{% load website %}\n{% get_homepage %}")
+    definition = parser.goto_definition(1, 14)
+    assert definition is not None
+    assert definition.uri == "file:///project/src/templatetags/website.py"
+    assert definition.range.start.line == 8
+    assert definition.range.start.character == 0
+
+
+@pytest.mark.parametrize(
+    "content",
+    [
+        """{% extends 'base.html' %}""",
+        """{% extends "base.html" %}""",
+        """{% include 'base.html' %}""",
+        """{% include "base.html" %}""",
+    ],
+)
+def test_goto_definition_template(content):
+    parser = create_parser(content)
+    definition = parser.goto_definition(0, 16)
+    assert definition is not None
+    assert definition.uri == "file:///project/src/templates/base.html"
+    assert definition.range.start.line == 0
+    assert definition.range.start.character == 0
